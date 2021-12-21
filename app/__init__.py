9fdde82fe308fb88a3ce4fd058bb114a614bf08f
Sam Davitt <samdavitt@Sams-MacBook-Pro.local>
2021-12-21 16:49:47 -0500

    basic auth functionality

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,10 @@ from config import Config
 from .api.routes import api
 from .auth.routes import auth
 
+# imports for our database stuff
+from .models import db, login
+from flask_migrate import Migrate
+
 # create/instantiate our flask object (create our flask app)
 app = Flask(__name__)
 # and then configure our flask app based on the Config class
@@ -14,5 +18,16 @@ app.config.from_object(Config)
 app.register_blueprint(api)
 app.register_blueprint(auth)
 
+# set up our ORM and Migrate connections
+db.init_app(app)
+migrate = Migrate(app, db)
+
+# config for our login manager
+login.init_app(app)
+login.login_view = 'auth.signin'
+
 # give this app/flask object access to it's routes
 from . import routes
+
+# give the app/flask object access to it's database models
+from . import models
--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,11 +1,75 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+# auth routes need to use forms, import those forms
+from app.forms import signupForm, signinForm
+
+# imports for working with our User model and signing users up and logins
+from app.models import db, User
+from flask_login import login_user, logout_user, current_user
+from werkzeug.security import check_password_hash
 
 auth = Blueprint('auth', __name__, template_folder='auth_templates', url_prefix='/auth')
 
-@auth.route('/signin')
+@auth.route('/signin', methods=['GET', 'POST'])
 def signin():
-    return render_template('signin.html')
+    form = signinForm() # used by both GET and POST
 
-@auth.route('/register')
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            print('This user is ready to be checked if they gave the right username and password')
+            print(form.username.data, form.password.data)
+            user = User.query.filter_by(username=form.username.data).first()
+            if user is None or not check_password_hash(user.password, form.password.data):
+                # username didn't exist or user gave us the wrong password
+                print('bad login attempt')
+                return redirect(url_for('auth.signin'))
+            # implied else -> the username and the password given matched a user in our database
+            login_user(user)
+            print(f'Thanks for logging in, {user.username}.')
+            return redirect(url_for('home'))
+        else:
+            # we have a bad form submission
+            print('Bad form input, try again')
+            return redirect(url_for('auth.signin'))
+
+    return render_template('signin.html', form=form) # works with GET requests
+
+@auth.route('/register', methods=['GET', 'POST'])
 def signup():
-    return render_template('signup.html')
+    # plan to use the signupForm here
+    form = signupForm()
+
+    # 2 scenarios
+        # GET - just render the template for the user
+        # POST - take in the submitted form info and do something with it
+    if request.method == 'POST': # if the user submitted the form
+        # ok, the user is trying to send us form info
+        # validate the form info
+        if form.validate_on_submit():
+            # we have proper user info - we want to create a user account
+            print('successful new user data received')
+            new_user = User(form.username.data, form.email.data, form.password.data, form.first_name.data, form.last_name.data)        
+            print(f'New user created - {new_user.__dict__}')
+            # try to upload that user to our database
+            #   now 2 things could go wrong - we said that username and email must be unique - if either is not unique, we get an error
+            try:
+                db.session.add(new_user)
+                db.session.commit()
+            except:
+                print('Username or email taken.')
+                return redirect(url_for('auth.signup'))
+            print('New user registered!')
+            login_user(new_user)
+            return redirect(url_for('home'))
+        else:
+            # we have a bad form submission
+            print('Bad form input, try again')
+            return redirect(url_for('auth.signup'))
+
+    return render_template('signup.html', form=form) # this return works for GET
+
+
+@auth.route('/logout')
+def logout():
+    logout_user()
+    print('Logged out.')
+    return redirect(url_for('auth.signin'))
